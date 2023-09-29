a1c48016f2f62db448ad4c9ad8ef91932e950cfc
aomsinn2000 <117340183+aomsinn2000@users.noreply.github.com>
2023-09-29 13:35:02 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <NavbarView></NavbarView>
+  <RouterView/>
+</template>
+
+
+
+
+
+<script setup lang="js">
+import NavbarView from './views/NavbarView.vue'
+
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Kanit:wght@100;200;300&display=swap');
+#app {
+  font-family: 'Kanit', sans-serif;
+}
+</style>
